1e8ba57f9b3c70b9451d105babc7083688a88007
barld <booba@hr.nl>
2023-10-09 18:38:05 +0200

    Use generics to reuse code in access layer

--- DataAccess/JsonAccessLayer.cs
+++ DataAccess/JsonAccessLayer.cs
@@ -1,9 +1,28 @@
 using System.Text.Json;
 
-public class JsonAccessLayer 
+public abstract class JsonAccessLayer<T> 
 {
     protected JsonSerializerOptions jsonOptions = new JsonSerializerOptions{
         WriteIndented = true,
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase
     };
+
+    protected abstract string path { get; }
+
+    public IEnumerable<T>? LoadAll()
+    {
+        string json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<IEnumerable<T>>(json, jsonOptions);
+    }
+
+
+    public void WriteAll(IEnumerable<T> data)
+    {
+        string json = JsonSerializer.Serialize(data, jsonOptions);
+        File.WriteAllText(path, json);
+    }
+
+    public void Add(T model) {
+        WriteAll(LoadAll().Append(model));
+    }
 }
